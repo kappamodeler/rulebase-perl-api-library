04f906ef6a36945f51a18e66e9300cc329bdfbf1
Brian Kaney <brian@vermonster.com>
2010-02-16 15:35:34 -0500

    Updated documentation.

--- lib/Cellucidate.pm
+++ lib/Cellucidate.pm
@@ -30,6 +30,7 @@ our $AUTH = {
 1;
 
 __END__
+
 =head1 NAME
 
 Cellucidate - Perl library for cellucidate.com
@@ -75,20 +76,14 @@ Cellucidate - Perl library for cellucidate.com
   $simulation_runs = Cellucidate::Model->simulation_runs( model_id );
 
   # Create a simulation run for a model
-  $simulation_run_params = {
-    "model_id" => $model_id,
-    "num_iterations" => 3
-  };
+  $simulation_run_params = { "model_id" => $model_id  };
   $simulation_run = Cellucidate::SimulationRun->create($simulation_run_params);
 
-  Cellucidate::SimulationRun->get($simulation_run->{id})->state; # is it still running?
+  $csv_data = Cellucidate::SimulationRun->get($simulation_run_id, 'csv');
 
+  $plots = Cellucidate::SimulationRun->plots($simulation_run_id);
 
   $plots = $simulation_run->plots();  # Cellucidate::Plot->find( { simulation_run_id => $id } );
-
-  /plots/1 (/plots1.xml) => metadata
-  /plots/1.csv => Dump of CSV data
-
 
 =head1 DESCRIPTION
 
@@ -100,22 +95,27 @@ hierarchy is:
 
   Bookshelves
   `-> Books
+      `-> Agents
+      `-> Rules
       `-> Models
           `-> Model Rules
           `-> Initial Conditions
+          `-> Rule Observables
+          `-> Solution Observables
           `-> Simulation Runs
+              `-> Ode Results
               `-> Plots
                   `-> Series
 
-Additionally, there are a few resources for auxiluary tasks, like importing
-data and running various jobs.
+Additionally, there are a few resources for auxiliary tasks, like importing
+data and running various jobs L<Cellucidate::KappaImportJob>.
 
 =head2 REST
 
 Cellucidate has a RESTful interface.  So this library is built upon the 
 L<REST::Client> library.  At the core, there are 4 operations over HTTP,
 including: GET, POST, PUT and DELETE.  The encoding is primarily XML, but
-certain resource can be returned in CSV.
+certain resource can be returned in CSV and Kappa (application/x-kappa).
 
 =head2 COMMON METHODS / PATTERN
 
@@ -135,18 +135,19 @@ subclasses.
 
 =over 4
 
-=item $Cellcuidate::CONFIG
+=item $Cellucidate::CONFIG
 
 This is a hash of configuration options for the underling REST client.
 See L<REST::Client> for more information.
+
+    $Cellucidate::CONFIG = { host => 'http://api.cellucidate.com' };
 
 =item $Cellucidate::AUTH
 
 This is a hash of your authentication information from cellucidate.com.
 It includes your email address as your login and API Key.
 
-  $Cellucidate::AUTH = { login => 'email@host.com', api_key => '111111' };
-
+    $Cellucidate::AUTH = { login => 'email@host.com', api_key => '111111' };
 
 =head1 SUBCLASSES
 
@@ -159,12 +160,24 @@ Base class for each resource.
 =item L<Cellucidate::Bookshelf> 
 
 Represents a Bookshelf in Cellucidate.  This is pretty much the top tier
-of resources.
+of cellucidate resources.
 
-=item L<Cellucidate::Bookshelf>
+=item L<Cellucidate::Book>
 
 Represents a Book in Cellucidate.  This is where Models live.  A book
 can be placed on a Bookshelf and has many Models.
+
+=item L<Cellucidate::Agent>
+
+Represents an agent in Cellucidate.  Agents are used by rules and initial
+conditions and solution observables.  Books have zero, one or many agents
+and an agent belongs to a book.
+
+=item L<Cellucidate::Rule>
+
+Represents a rule in Cellucidate. Books have zero, one or many rules
+and a rule belongs to a book.  Models have Rules through the ModelRule
+resource.
 
 =item L<Cellucidate::Model> 
 
@@ -175,20 +188,35 @@ simulations can be run from a Model, each having a number of settings.
 =item L<Cellucidate::ModelRule> 
 
 Represents a Rule that is used my a model.  Model rules belong to a model and
-models have one or more typically many model rules.
+models have one or more typically many model rules.  ModelRules have an
+association to Rules.
 
+=item L<Cellucidate::SolutionObservable>
+
+Represents an Agent or set of Agents that you want to observe and plot the 
+concentration of when the simulation is run.
+
+=item L<Cellucidate::RuleObservable>
+
+Represents a ModelRule that you want to observe the activity of when the
+simulation is run.
 
 =item L<Cellucidate::InitialCondition>
 
 This is an initial condition in a model.  Models have one by more typically
-many initial conditions and an initial condition belongs to a rule.
+many initial conditions and an initial condition belongs to a model.
 
 =item L<Cellucidate::SimulationRun>
 
-A simulation run is a simulation using the components from a model.  A
-model has one or many simulation runs and a simulation run belongs to
-a model.  A simulation run has the settings as attributes for a
-simulation.
+A simulation run represents an execution of the Cellucidate simulator and contains
+child result resources.  Simulation runs define various settings.
+A Model has one or many simulation runs and a simulation run belongs to
+a model.
+
+=item L<Cellucidate::OdeResult>
+
+Represents the set of ODE formulas generated when running a simulation in ODE-mode.
+The results can be used directly in MATLAB.
 
 =item L<Cellucidate::Plot>
 
@@ -201,13 +229,11 @@ each iteration).
 A collection of datapoints.  A series belongs to a plot and represents an observable
 or time.
 
-
 =item L<Cellucidate::KappaImportJob>
 
-This is a resource that represents an import job.  This allow the creation
+This is a resource that represents an import job.  This allows the creation
 of a Book, including Model Rules and Initial Conditions, from a Kappa 
 string.
-
 
 =item L<Cellucidate::Request>
 
@@ -226,7 +252,7 @@ Brian Kaney, E<lt>brian@vermonster.com<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2010 by Brian Kaney
+Copyright (C) 2010 by Plectix BioSystems Inc 
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.9 or,
